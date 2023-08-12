01f8b15d3a79344be8a446521ff1709e9d81b284
Anas Awal <a.awal@big-bang.ae>
2023-08-12 19:19:03 +0300

    changes for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
 import Home from './view/Home';
-import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <HashRouter base="/">
-        <Home />
-      </HashRouter>
+      <Home />
     </div>
   );
 }
